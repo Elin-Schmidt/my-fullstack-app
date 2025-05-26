3fd812551d23084db50551ebb9509dfa0f9beca7
Elin <elin.schmidt@gmail.com>
2025-05-26 13:22:26 +0200

    Refactor code structure for improved readability and maintainability

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,7 +20,7 @@ app.use(
             process.env.NODE_ENV === 'production'
                 ? 'https://boply.onrender.com'
                 : 'http://localhost:5173',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Lägg till PATCH här!
         credentials: true
     })
 );
