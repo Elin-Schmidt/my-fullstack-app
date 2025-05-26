a36a2a922683a62e7005a0ba0d779152fb1e1d57
Elin <elin.schmidt@gmail.com>
2025-05-26 10:54:57 +0200

    fix: Move express.json() middleware to the correct position in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,12 +8,11 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.get('/test', (req, res) => {
     res.send('Root test works!');
 });
-
-app.use(express.json());
 
 app.use(
     cors({
